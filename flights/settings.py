c04ac43d7399e9de830b0381c97668a402b94626
Pavel Fokin <fokinpv@gmail.com>
2020-09-19 12:09:43 +0300

    Additional requirements for heroku

--- flights/settings.py
+++ flights/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 import os
 from pathlib import Path
 
+import dj_database_url
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -146,7 +148,8 @@ SWAGGER_SETTINGS = {
     }
 }
 
-# Activate Django-Heroku.
-if not DEBUG:
-    import django_heroku
-    django_heroku.settings(locals())
+db_config = dj_database_url.config(
+    conn_max_age=600, ssl_require=True
+)
+if db_config:
+    DATABASES["default"] = db_config
